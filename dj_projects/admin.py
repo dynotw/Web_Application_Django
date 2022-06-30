3864410528618b59c95bee3e3d92083931e39a5f
Daniel Dong <dongyn1996@gmail.com>
2022-06-30 19:02:57 +0800

    Add all files

---
+++ dj_projects/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from dj_projects.models import Topic, Entry
+
+
+# Register your models here.
+admin.site.register(Topic)
+admin.site.register(Entry)
+
---
+++ dj_projects/urls.py
@@ -0,0 +1,15 @@
+"""
+Define dj_projects URL mode
+"""
+
+from django.urls import path
+from . import views
+
+# This app_name helps Django distinguish urls.py from others from other App
+app_name = 'dj_projects'
+# This urlpatterns is a list of URL which can be requested from dj_projects App
+urlpatterns = [
+    # Home
+    # 1.URL; 2.View function (from views.py); 3.Template (index.html)
+    path('', views.index, name='index'),
+]
