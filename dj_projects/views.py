cf82cf4a60768a397457594b0022c52f4516382e
Daniel Dong <dongyn1996@gmail.com>
2022-07-02 21:02:12 +0800

    restrict user to own data

--- dj_projects/views.py
+++ dj_projects/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 # Create your views here.
 def index(request):
@@ -8,23 +10,29 @@ def index(request):
     return render(request, 'dj_projects/index.html')
 
 # views function that Show all topics
+@login_required # a decorator provided by Django, checking whether a user is logged in
 def topics(request):
     """
     Show all topics
     URL: http://localhost:8000/topics/
     """
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     context = {'topics': topics}
     return render(request, 'dj_projects/topics.html', context)
 
 # Views function that show an individual topic, based on topic_id
+@login_required
 def topic(request, topic_id):
     topic = Topic.objects.get(id = topic_id)
+    # Make sure the topic belongs to the current user
+    if topic.owner != request.user:
+        raise Http404
     entries = topic.entry_set.order_by('-date_added') # '-' this minus sign means reverse order
     context = {'topic': topic, 'entries': entries}
     return render(request, 'dj_projects/topic.html', context)
 
 # Views function for Page which adds new topic not in admin page
+@login_required
 def new_topic(request):
     """Add a new topic"""
     if request.method != "POST":
@@ -34,12 +42,15 @@ def new_topic(request):
         # POST data submitted , then process data
         form = TopicForm(data=request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return redirect('dj_projects:topics')
     context = {'form': form}
     return render(request, 'dj_projects/new_topic.html', context)
 
 # Views function for Page which adds new entry for topic_id
+@login_required
 def new_entry(request, topic_id):
     """Add a new entry for a topic with topic_id"""
     topic = Topic.objects.get(id=topic_id)
@@ -60,10 +71,13 @@ def new_entry(request, topic_id):
     return render(request, 'dj_projects/new_entry.html', context)
 
 # Views function for editing entry with entry_id
+@login_required
 def edit_entry(request, entry_id):
     """Edit an existed entry"""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+    if topic.owner != request.user:
+        raise Http404
     if request.method != "POST":
         # Initial request, pre-fill form with the current entry
         form = EntryForm(instance=entry, data=request.POST)
