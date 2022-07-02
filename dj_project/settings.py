1cdbd0e185507b8870755772c9577f52b7e06e24
Daniel Dong <dongyn1996@gmail.com>
2022-07-02 21:17:32 +0800

    change the order in setting.py of INSTALLED_APPS

--- dj_project/settings.py
+++ dj_project/settings.py
@@ -31,16 +31,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Add my own app
+    'dj_projects',
+    'users',
+
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    # Add my own app
-    'dj_projects',
-    'users'
 ]
 
 MIDDLEWARE = [
